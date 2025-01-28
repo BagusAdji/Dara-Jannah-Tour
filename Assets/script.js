fdda9f10f7295c75f6b27ec286ac61ffdb83d1bb
BagusAdji <bagusadjirohman@gmail.com>
2025-01-28 18:40:18 +0700

    navbar fixed

--- Assets/script.js
+++ Assets/script.js
@@ -58,3 +58,18 @@ document.querySelectorAll('.slider-container').forEach((slider) => {
         }
     });
 });
+
+const toggleBtn = document.querySelector('.toggle_btn')
+       const toggleBtnIcon = document.querySelector('.toggle_btn i')
+       const dropDownMenu = document.querySelector('.dropdown_menu')
+       const Container = document.querySelector('.container')
+
+       toggleBtn.onclick = function (){
+        dropDownMenu.classList.toggle('open')
+        const isOpen = dropDownMenu.classList.contains('open')
+        Container.classList.toggle('open')
+
+        toggleBtnIcon.classList = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars'
+       }
