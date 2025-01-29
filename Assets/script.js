0dbd7906c6a29596c595344b573968b6c98c72cc
BagusAdji <bagusadjirohman@gmail.com>
2025-01-29 12:35:02 +0700

    fix bug

--- Assets/script.js
+++ Assets/script.js
@@ -78,3 +78,22 @@ document.addEventListener("DOMContentLoaded", function () {
             : 'fa-solid fa-bars';
     };
 });
+
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.carousel img');
+
+function showImage(index) {
+    images.forEach((img, i) => {
+        img.classList.toggle('active', i === index);
+    });
+}
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    showImage(currentImageIndex);
+}
+
+function prevImage() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    showImage(currentImageIndex);
+}
